ef3fb0264a1885bd62d6099f9bf868717ce7a44e
Yauheni <eluki2009@yandex.ru>
2022-09-15 16:57:42 +0300

    feature-sliced design

--- config/build/types/config.ts
+++ config/build/types/config.ts
@@ -9,6 +9,7 @@ export interface BuildPaths {
     entry: string;
     build: string;
     html: string;
+    src: string;
 
 };
 export interface BuildOptions {
@@ -16,6 +17,7 @@ export interface BuildOptions {
     paths: BuildPaths;
     isDev: boolean;
     port: number;
+
 
 };
 
--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -1,15 +1,12 @@
 import React, { FC, useMemo, useState } from 'react'
-import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from './ThemeContext'
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from '../lib/ThemeContext';
+
 
 
 const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT;
 const ThemeProvider : FC = ({children}) => {
     const [theme, setTheme] = useState<Theme>(defaultTheme);
-  const toggleTheme = () => {
-
-    setTheme( theme == Theme.LIGHT ? Theme.DARK : Theme.LIGHT )
-
-  }
+  
   const defaultProps = useMemo( () => ({
     theme: theme,
     setTheme: setTheme
