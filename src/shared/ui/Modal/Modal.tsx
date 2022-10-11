d5bd39f4947f9f1f6cd1370ba47c5b4241e543f0
Yauheni <eluki2009@yandex.ru>
2022-10-11 11:13:55 +0300

    fix theme form modal

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -29,7 +29,6 @@ export const Modal = (props:ModalProps) => {
     const mods: Record<string, boolean> = {
         [cls.opened]: isOpen,
         [cls.isClosing]: isClosing,
-        [cls[theme]]: true,
     };
 
     const timerRef = useRef<ReturnType<typeof setTimeout>>();
@@ -67,7 +66,7 @@ export const Modal = (props:ModalProps) => {
 
     return (
         <Portal>
-            <div className={classNames(cls.Modal, mods, [className])}>
+            <div className={classNames(cls.Modal, mods, [className, theme])}>
                 <div className={cls.overlay} onClick={closeHandler}>
                     <div className={cls.content} onClick={onContentClick}>
                         {children}
