c32b5558d1abbf4096ed827f4b5b6b652f762cfa
Yauheni <eluki2009@yandex.ru>
2022-12-03 15:53:31 +0300

    fix styles problems

--- src/entities/Comment/ui/CommentCard/CommentCard.tsx
+++ src/entities/Comment/ui/CommentCard/CommentCard.tsx
@@ -21,13 +21,13 @@ export const CommentCard = memo((props: CommentCardProps) => {
 
     if (isLoading) {
         return (
-            <div className={classNames(cls.CommentCard, {}, [className, cls.loading])}>
+            <VStack gap="8" max className={classNames(cls.CommentCard, {}, [className, cls.loading])}>
                 <div className={cls.header}>
                     <Skeleton width={30} height={30} border="50%" />
                     <Skeleton width={200} height={30} className={cls.username} />
                 </div>
                 <Skeleton width="100%" height={100} className={cls.text} />
-            </div>
+            </VStack>
         );
     }
 
